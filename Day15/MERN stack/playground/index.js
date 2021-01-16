accb95aeea6225b95efcef83d5d7b6c8ed46c8f1
OliviaMin <52757882+OliviaMin@users.noreply.github.com>
2021-01-16 15:03:21 +0530

    Update index.js

--- Day15/MERN stack/playground/index.js
+++ Day15/MERN stack/playground/index.js
@@ -2,6 +2,8 @@ const path=require("path")
 const express=require('express')
 const cors=require('cors')
 const bodyParser=require('body-parser')
+
+const errorHandler=require('./middleware/errorHandler');
 const postRoutes=require("./routes/post")
 const app=express();
 
@@ -9,7 +11,9 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname,"public")));
 
+
 app.use("/api/post",postRoutes)
+app.use(errorHandler)
 
 app.listen(8000,()=>{
     console.log("Listening..");
