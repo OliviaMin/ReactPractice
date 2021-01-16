011ba66cfaf4e43d1e612bb099b4cb913d88f2eb
OliviaMin <52757882+OliviaMin@users.noreply.github.com>
2021-01-16 14:57:20 +0530

    Update postController.js

--- Day15/MERN stack/playground/controllers/postController.js
+++ Day15/MERN stack/playground/controllers/postController.js
@@ -1,3 +1,14 @@
+const validationHandler=require('../validations/validationHandle')
+
 exports.index=(req,res)=>{
     res.send({message: "hi"});
+}
+
+exports.store=(req,res,next)=>{
+try{
+validationHandler(req);
+res.send({message: "The name is" + req.body.name});
+}catch(err){
+    next(err);
+}
 }
