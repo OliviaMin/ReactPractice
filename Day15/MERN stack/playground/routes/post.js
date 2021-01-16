55b9baec1cbbd77e160e393e11455fcd4af2070f
OliviaMin <52757882+OliviaMin@users.noreply.github.com>
2021-01-16 14:58:46 +0530

    Update post.js

--- Day15/MERN stack/playground/routes/post.js
+++ Day15/MERN stack/playground/routes/post.js
@@ -1,6 +1,10 @@
 const express=require('express')
 const postController=require("../controllers/postController")
 const router=express.Router();
+const {hasName}=require('../validations/validators')
+
 router.get('/',postController.index);
+router.post('/',hasName,postController.store);
+
 
 module.exports=router;
